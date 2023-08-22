4a43a89777281e44f1d5cd0b5ed0a9f759c8bc12
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-08-22 19:39:47 +0500

    finished crud operations

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -5,14 +5,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import uz.pdp.appjparelationships.entity.Address;
+import uz.pdp.appjparelationships.entity.Group;
 import uz.pdp.appjparelationships.entity.Student;
+import uz.pdp.appjparelationships.payload.StudentDto;
+import uz.pdp.appjparelationships.repository.GroupRepository;
 import uz.pdp.appjparelationships.repository.StudentRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
     @Autowired
     StudentRepository studentRepository;
+    @Autowired
+    GroupRepository groupRepository;
 
     //1. VAZIRLIK
     @GetMapping("/forMinistry")
@@ -44,5 +52,70 @@ public class StudentController {
     //3. FACULTY DEKANAT
     //4. GROUP OWNER
 
+    @PostMapping
+    public String createStudent(@RequestBody StudentDto dto) {
+        Optional<Group> group = groupRepository.findById(dto.getGroupId());
+        if (!group.isPresent()) {
+            return "Error. Group not found with id = " + dto.getGroupId();
+        }
+        Student student = new Student();
+        Address address = new Address();
+        address.setCity(dto.getCity());
+        address.setDistrict(dto.getDistrict());
+        address.setStreet(dto.getStreet());
 
+        student.setFirstName(dto.getFirstName());
+        student.setLastName(dto.getLastName());
+        student.setAddress(address);
+        student.setGroup(group.get());
+
+        Student studentDB = studentRepository.save(student);
+
+        return "Student saved with id = " + studentDB.getId();
+    }
+
+    @PutMapping("/{id}")
+    public String updateStudent(@RequestBody StudentDto dto, @PathVariable Integer id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (!optionalStudent.isPresent()) {
+            return "Error. Student not found with id = " + id;
+        }
+        Student student = optionalStudent.get();
+        if (dto.getGroupId() != null) {
+            Optional<Group> group = groupRepository.findById(dto.getGroupId());
+            if (!group.isPresent()) {
+                return "Error. Group not found with id = " + dto.getGroupId();
+            }
+            student.setGroup(group.get());
+        }
+        Address address = student.getAddress();
+        if (dto.getCity() != null) {
+            address.setCity(dto.getCity());
+        }
+        if (dto.getDistrict() != null) {
+            address.setDistrict(dto.getDistrict());
+        }
+        if (dto.getStreet() != null) {
+            address.setStreet(dto.getStreet());
+        }
+        if (dto.getFirstName() != null) {
+            student.setFirstName(dto.getFirstName());
+        }
+        if (dto.getLastName() != null) {
+            student.setLastName(dto.getLastName());
+        }
+
+        studentRepository.save(student);
+
+        return "Student successfully updated with id = " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Integer id) {
+        if (studentRepository.existsById(id)) {
+            studentRepository.deleteById(id);
+            return "Student successfully deleted with id = " + id;
+        }
+        return "Error. Student not found with id = " + id;
+    }
 }
